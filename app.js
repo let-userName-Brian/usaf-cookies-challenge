814b755b89a27eb8a6a5a111a3245e5c02ceb480
Brian <brian.hardy67@gmail.com>
2021-10-22 15:08:17 -0400

    working

---
+++ app.js
@@ -0,0 +1,25 @@
+//create express app    
+const express = require('express');
+const app = express();
+
+const morgan = require('morgan');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })) 
+app.use(morgan('dev'));
+
+
+//Create an Express application that sets a cookie when routed to /login with their name
+app.get('/login', (req, res) => {
+    res.cookie('name', req.params.name);
+});
+
+
+//If a cookie is present with a name key, then it says "Welcome {name}! when the user routes to /hello"
+app.get('/hello', (req, res) => {
+    if (req.cookies.cookie) {
+        res.send(`Welcome ${req.cookies.cookie}!`);
+    } else {
+        res.send('Please login');
+    }
+});
